f88d71a094df2ff6ad2d4b9039db8fcd38e1dc98
bccasagrande <b.ccasagrandee@gmail.com>
2026-07-08 12:51:13 -0300

    fix: ajusta icone de perfil e tamanho da logo na navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -15,7 +15,12 @@ function Navbar() {
 
       <div className="navbar-perfil">
         <a href="/perfil">Perfil</a>
-        <span className="navbar-perfil-icon">&#9776;</span>
+        <span className="navbar-perfil-icon">
+          <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <circle cx="12" cy="8" r="4" stroke="currentColor" strokeWidth="2"/>
+            <path d="M4 20c0-4.4 3.6-8 8-8s8 3.6 8 8" stroke="currentColor" strokeWidth="2" strokeLinecap="round"/>
+          </svg>
+        </span>
       </div>
     </header>
   );
